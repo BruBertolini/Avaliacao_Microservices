53156e2fb940ed8f7437835f84d376db4fd2f1aa
Bruna Bertolini <bbcorreia12@hotmail.com>
2019-04-30 08:29:52 -0300

    Update TransactionDao.java

--- src/main/java/br/com/fiap/transaction/persistance/TransactionDao.java
+++ src/main/java/br/com/fiap/transaction/persistance/TransactionDao.java
@@ -3,8 +3,6 @@ package br.com.fiap.transaction.persistance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-
 import br.com.fiap.transaction.bean.Transaction;
 import br.com.fiap.transaction.service.StatisticsService;
 import br.com.fiap.transaction.service.TransactionService;
